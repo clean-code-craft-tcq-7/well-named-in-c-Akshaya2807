90dca61e4270d1cb39d602e317cc2fb3c0d99dee
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-11-20 11:45:08 +0530

    Update Colorcoding.h

--- Colorcoding.h
+++ Colorcoding.h
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-const int MAX_COLORPAIR_NAME_CHARS = 16;
 
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 typedef struct {
     enum MajorColor majorColor;
