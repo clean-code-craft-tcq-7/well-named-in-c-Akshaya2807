8e9536ffc6b72af1cc542ef455e089b0b100e1e1
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-11-20 13:19:59 +0530

    Update main.c

--- main.c
+++ main.c
@@ -34,10 +34,10 @@ int main() {
     printf("PRINTING MANUAL\n");
     for(int i =1 ;i<26;i++)
     {
-     colorPair = GetColorFromPairNumber(i);
-     printf("majorcolor-%s,minorcolor-%s,%d-pairNumber\n",MajorColorNames[colorPair.majorColor],MajorColorNames[colorPair.minorColor],i);
+        colorPair = GetColorFromPairNumber(i);
+        printf("majorcolor-%s,minorcolor-%s,%d-pairNumber\n",MajorColorNames[colorPair.majorColor],MajorColorNames[colorPair.minorColor],i);
     }
-   printf("DO TEST VERIFICATION\n");
+    printf("DO TEST VERIFICATION\n");
    testNumberToPair(4, WHITE, BROWN);
    testNumberToPair(5, WHITE, SLATE);
 
