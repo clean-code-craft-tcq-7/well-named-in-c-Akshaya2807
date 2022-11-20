77bc319553a160108b04a144d049f7b49b7785a9
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-11-20 12:11:07 +0530

    Update Colorcoding.h

--- Colorcoding.h
+++ Colorcoding.h
@@ -21,3 +21,10 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
