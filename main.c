081c69a6be9b081f3fa0a0bac67eda6ea0b7a3d2
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2022-11-20 13:05:11 +0530

    Update main.c

--- main.c
+++ main.c
@@ -30,11 +30,19 @@ void testPairToNumber(
 }
 
 int main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
+    ColorPair colorPair;
+    printf("PRINTING MANUAL\n");
+    for(int i =1 ;i<26;i++)
+    {
+   colorPair = GetColorFromPairNumber(i);
+   printf("majorcolor-%s,minorcolor-%s,%d-pairNumber\n",MajorColorNames[colorPair.majorColor],MajorColorNames[colorPair.minorColor],i);
+    }
+   printf("DO TEST VERIFICATION\n");
+   testNumberToPair(4, WHITE, BROWN);
+   testNumberToPair(5, WHITE, SLATE);
 
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
+   testPairToNumber(BLACK, ORANGE, 12);
+   testPairToNumber(VIOLET, SLATE, 25);
 
     return 0;
 }
